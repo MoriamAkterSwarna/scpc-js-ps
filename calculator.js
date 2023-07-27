bb88bb235fde649af4c3b0df2b384c3c087dd156
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2023-07-27 11:10:36 +0600

    calculator added

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function calculator(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+        case '%':
+            return num1 % num2;
+        default:
+            return 'Invalid operator';
+    }
+}
+
+console.log(calculator(5, 3, '+'));
+console.log(calculator(5, 3, '-')); 
+console.log(calculator(5, 3, '*')); 
+console.log(calculator(5, 3, '/')); 
+console.log(calculator(5, 3, '%')); 
+console.log(calculator(5, 3, '$')); 
